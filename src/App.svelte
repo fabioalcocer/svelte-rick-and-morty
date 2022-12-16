ace95fd148a9e68090e20f487c582bc2cbf3cf74
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-12-15 23:09:03 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  let characters = []
+
+  const loadCharacters = async () => {
+    const res = await fetch(
+      'https://rickandmortyapi.com/api/character'
+    )
+    const data = await res.json()
+    characters = data.results
+  }
+
+  loadCharacters()
+</script>
+
+<h1>Rick and Morty Svelte Practice</h1>
+
+<div>
+  {#each characters as character}
+    <div>
+      <img src={character.image} alt={character.name} />
+      <h2>{character.name}</h2>
+      <p>{character.species}</p>
+    </div>
+  {/each}
+</div>

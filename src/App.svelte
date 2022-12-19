5b110f8a2113ec071bca40977de4eef22bec072e
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-12-19 17:53:34 -0400

    add pagination and grid

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,41 @@
 <script>
+  import Character from './lib/Character.svelte'
+
   let characters = []
+  let page = 1
 
   const loadCharacters = async () => {
     const res = await fetch(
-      'https://rickandmortyapi.com/api/character'
+      `https://rickandmortyapi.com/api/character?page=${page}`
     )
     const data = await res.json()
     characters = data.results
   }
 
+  const nextPage = () => {
+    page++
+    loadCharacters()
+  }
+
+  const prevPage = () => {
+    page--
+    loadCharacters()
+  }
+
   loadCharacters()
 </script>
 
-<h1>Rick and Morty Svelte Practice</h1>
+<h1 class="title">Rick and Morty Svelte Practice</h1>
 
-<div>
-  {#each characters as character}
-    <div>
-      <img src={character.image} alt={character.name} />
-      <h2>{character.name}</h2>
-      <p>{character.species}</p>
-    </div>
-  {/each}
+<div class="container">
+  <div class="btns">
+    <button class="btn" on:click={prevPage} disabled={page === 1}>Previous</button
+    >
+    <button class="btn" on:click={nextPage}>Next</button>
+  </div>
+  <div class="grid">
+    {#each characters as character}
+      <Character {character} />
+    {/each}
+  </div>
 </div>
